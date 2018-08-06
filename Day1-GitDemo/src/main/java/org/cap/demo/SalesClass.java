906b991b95380d4a5e1eda7b938cc6eeb23adf75
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2018-08-06 14:30:21 +0530

    Show method with normal for loop.

--- Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -4,8 +4,8 @@ public class SalesClass {
 	int[] arr= {1,2,3,-89,56};
 	
 	public void show() {
-		for(int num:arr)
-			System.out.println(num);
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
 	}
 	
 	public void printLoop() {
