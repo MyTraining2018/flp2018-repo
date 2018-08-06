1021dce9384797940e981b18f50be40fceac8698
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2018-08-06 14:28:36 +0530

    Show method included with sum.

--- Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -4,8 +4,12 @@ public class SalesClass {
 	int[] arr= {1,2,3,-89,56};
 	
 	public void show() {
-		for(int num:arr)
+		int sum=0;
+		for(int num:arr) {
+			sum+=num;
 			System.out.println(num);
+		}
+		System.out.println(sum);
 	}
 	
 	public void printLoop() {
